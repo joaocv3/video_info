f30c2ba571ca8cc38a0226717cd25c7ab1261728
Joao Vieira <joaocv3@gmail.com>
2020-10-01 21:53:51 -0300

    Fix Youtube Scraper spec by setting provider api keys as empty hash

--- spec/lib/video_info/providers/youtube_scraper_spec.rb
+++ spec/lib/video_info/providers/youtube_scraper_spec.rb
@@ -4,6 +4,8 @@ describe VideoInfo::Providers::Youtube do
   context 'with a video without description', :vcr do
     subject { VideoInfo.new('http://www.youtube.com/watch?v=WVsj2pS-zFY') }
 
+    before { VideoInfo.provider_api_keys = {} }
+
     describe '#description' do
       subject { super().description }
       it { is_expected.to eq " " }
